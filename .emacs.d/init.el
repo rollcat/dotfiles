73d2fc92a01989a0b2c80fe0c3fd359b531ffe74
Kamil Cholewinski <kamil@rollc.at>
2022-07-11 13:25:19 +0200

    emacs: prefer melpa-stable

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -71,9 +71,11 @@
 (require 'package)
 
 (setq package-archives
-      '(("org"   . "https://orgmode.org/elpa/")
-        ("gnu"   . "https://elpa.gnu.org/packages/")
-        ("melpa" . "https://melpa.org/packages/")))
+      '(("org"          . "https://orgmode.org/elpa/")
+        ("gnu"          . "https://elpa.gnu.org/packages/")
+        ("melpa-stable" . "https://stable.melpa.org/packages/")
+        ("melpa"        . "https://melpa.org/packages/")
+        ))
 
 (package-initialize)
 (unless package-archive-contents
